54dda99859044c94256a8e4f132157a48a881939
Mark Eissler <mark@mixtur.com>
2017-07-11 16:02:01 -0700

    GVM2-2 #comment Refactor gvm_environment_sanitize() to fix broken system and system@ environments.

---
+++ tests/func_environment_sanitize_comment_test.sh
@@ -0,0 +1,22 @@
+g_path_script="$(builtin cd "$(dirname "${BASH_SOURCE[0]}")" && /bin/pwd)"
+. "${g_path_script}/../scripts/function/gvm_environment_sanitize" || return 1
+
+##
+## sanitize system environment
+##
+
+## Setup expectation
+
+## Execute command
+
+## Evaluate result
+
+##
+## sanitize system@global environment
+##
+
+## Setup expectation
+
+## Execute command
+
+## Evaluate result
