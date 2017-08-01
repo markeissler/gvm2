1d88320735acc3eb02a0639e83e43348dfdfa524
Mark Eissler <mark@mixtur.com>
2017-07-31 19:50:06 -0700

    GVM2-18 #comment Move extract_version() to __gvm_extract_version() in semver_tools.sh.

--- scripts/function/semver_tools.sh
+++ scripts/function/semver_tools.sh
@@ -27,8 +27,8 @@
 #   2 - version_A is less than version_B
 # @param version_A Version string to compare on left
 # @param version_B Version string to compare on right
-# @return Returns comparison value between 0 and 2 on success or 65 on failure.
-#   Also sets global variable RETVAL to the same exit value.
+# @return Returns comparison value between 0 and 2.
+# @note Also sets global variable RETVAL to the same exit value.
 # */
 __gvm_compare_versions()
 {
@@ -78,4 +78,41 @@ __gvm_compare_versions()
     unset _i
 
 	RETVAL=0 && return 0
+}
+
+# __gvm_extract_version()
+# /*!
+# @abstract Strip extraneous SemVer string information.
+# @discussion
+# Removes extra SemVer information from a Go version string including the
+#   following substrings:
+# <pre>@textblock
+#   + beta
+#   + rc
+# @/textblock</pre>
+# Old pre-release versions will be return an error  to a SemVer value of 0.0.1 as they are
+#   irrelevant at this point. These versions include tags that begin with:
+# <pre>@textblock
+#   + release
+#   + weekly
+# @/textblock</pre>
+# @param version Go version string to strip
+# @return Returns string containing stripped value on success (status 0) or an
+#   empty string on failure (status 1).
+# @note  Also sets global variable RETVAL to the return string.
+# */
+__gvm_extract_version()
+{
+    local version="${1}"
+    unset RETVAL
+
+    [[ "x${version}" == "x" ]] && RETVAL="" && echo "${RETVAL}" && return 1
+
+    # weekly* and release* tags are really really old pre-release versions, we
+    # will just return an error for these.
+    [[ "${version}" =~ release* || "${version}" =~ weekly* ]] && RETVAL="" && echo "${RETVAL}" && return 1
+
+    RETVAL="$(echo "${version}" | sed 's/^go\(.*\)/\1/g; s/beta.*//g; s/rc.*//g')"
+
+    echo "${RETVAL}" && return 0
 }
---
+++ tests/func_extract_version_comment_test.sh
@@ -0,0 +1,67 @@
+. "${SANDBOX}/gvm2/scripts/function/semver_tools.sh" || return 1
+
+##
+## strip extraneous SemVer strings from a Go version string
+##
+
+## Setup expectation (left, right, retval)
+expectedVersion_1=( "go1.2.1"           "1.2.1"  "0" )
+expectedVersion_2=( "go1.3beta1"        "1.3"    "0" )
+expectedVersion_3=( "go1.6rc1"          "1.6"    "0" )
+expectedVersion_4=( "release.r57.1"     ""       "1" )
+expectedVersion_5=( "weekly.2011-12-01" ""       "1" )
+
+##
+## extract SemVer string (return status)
+##
+
+## Execute command
+extractedStatus_1="$(__gvm_extract_version "${expectedVersion_1[0]}" >/dev/null; echo $?)"
+extractedStatus_2="$(__gvm_extract_version "${expectedVersion_2[0]}" >/dev/null; echo $?)"
+extractedStatus_3="$(__gvm_extract_version "${expectedVersion_3[0]}" >/dev/null; echo $?)"
+extractedStatus_4="$(__gvm_extract_version "${expectedVersion_4[0]}" >/dev/null; echo $?)"
+extractedStatus_5="$(__gvm_extract_version "${expectedVersion_5[0]}" >/dev/null; echo $?)"
+
+## Evaluate result
+[[ "${extractedStatus_1}" -eq "${expectedVersion_2[2]}" ]] # status=0
+[[ "${extractedStatus_2}" -eq "${expectedVersion_2[2]}" ]] # status=0
+[[ "${extractedStatus_3}" -eq "${expectedVersion_3[2]}" ]] # status=0
+[[ "${extractedStatus_4}" -eq "${expectedVersion_4[2]}" ]] # status=0
+[[ "${extractedStatus_5}" -eq "${expectedVersion_5[2]}" ]] # status=0
+
+##
+## extract SemVer string (return value)
+##
+
+## Execute command
+extractedStatus_1="$(__gvm_extract_version "${expectedVersion_1[0]}")"
+extractedStatus_2="$(__gvm_extract_version "${expectedVersion_2[0]}")"
+extractedStatus_3="$(__gvm_extract_version "${expectedVersion_3[0]}")"
+extractedStatus_4="$(__gvm_extract_version "${expectedVersion_4[0]}")"
+extractedStatus_5="$(__gvm_extract_version "${expectedVersion_5[0]}")"
+
+## Evaluate result
+
+[[ "${extractedStatus_1}" == "${expectedVersion_1[1]}" ]] # status=0
+[[ "${extractedStatus_2}" == "${expectedVersion_2[1]}" ]] # status=0
+[[ "${extractedStatus_3}" == "${expectedVersion_3[1]}" ]] # status=0
+[[ "${extractedStatus_4}" == "${expectedVersion_4[1]}" ]] # status=0
+[[ "${extractedStatus_5}" == "${expectedVersion_5[1]}" ]] # status=0
+
+##
+## extract SemVer string (RETVAL value)
+##
+
+## Execute command
+extractedStatus_1="$(__gvm_extract_version "${expectedVersion_1[0]}" >/dev/null; echo "${RETVAL}")"
+extractedStatus_2="$(__gvm_extract_version "${expectedVersion_2[0]}" >/dev/null; echo "${RETVAL}")"
+extractedStatus_3="$(__gvm_extract_version "${expectedVersion_3[0]}" >/dev/null; echo "${RETVAL}")"
+extractedStatus_4="$(__gvm_extract_version "${expectedVersion_4[0]}" >/dev/null; echo "${RETVAL}")"
+extractedStatus_5="$(__gvm_extract_version "${expectedVersion_5[0]}" >/dev/null; echo "${RETVAL}")"
+
+## Evaluate result
+[[ "${extractedStatus_1}" == "${expectedVersion_1[1]}" ]] # status=0
+[[ "${extractedStatus_2}" == "${expectedVersion_2[1]}" ]] # status=0
+[[ "${extractedStatus_3}" == "${expectedVersion_3[1]}" ]] # status=0
+[[ "${extractedStatus_4}" == "${expectedVersion_4[1]}" ]] # status=0
+[[ "${extractedStatus_5}" == "${expectedVersion_5[1]}" ]] # status=0
