dd9e6e3cb8f73096984a7b6d19bbf49c9598b480
Mark Eissler <mark@mixtur.com>
2017-08-05 20:28:09 -0700

    GVM2-18 #comment Refactor scripts/alias-delete.

--- tests/gvm_alias_comment_test.sh
+++ tests/gvm_alias_comment_test.sh
@@ -8,6 +8,10 @@ gvm alias delete bar
 ## Test output messages
 gvm alias # status=1; match=/Unrecognized command: empty/
 gvm alias --help # status=0; match=/Usage: gvm alias <command> \[option\]/
+gvm alias create # status=1; match=/Unrecognized command: empty/
+gvm alias create --help # status=0; match=/Usage: gvm alias create \[options\] <alias-name>/
+gvm alias delete # status=1; match=/Unrecognized command: empty/
+gvm alias delete --help # status=0; match=/Usage: gvm alias delete \[options\] <alias-name>/
 
 gvm alias create foo go1.3.3 # status=0
 gvm alias create bar go1.2.2 # status=0
