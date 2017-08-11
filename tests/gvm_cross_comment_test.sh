f1b32241123a090ddd29f2dd6d4c544ad1b65151
Mark Eissler <mark@mixtur.com>
2017-08-10 23:48:49 -0700

    GVM2-18 #comment Refactor scripts/cross.

---
+++ tests/gvm_cross_comment_test.sh
@@ -0,0 +1,28 @@
+source "${SANDBOX}/gvm2/scripts/env/use.sh" || return 1
+
+##
+## Test output messages
+##
+
+gvm cross # status=1; match=/ERROR: Please specify the cross compilation platform operating system/
+gvm cross -h # status=0; match=/Usage: gvm cross \[option\] <os> <arch> \[sub-option\] <file ...>/
+gvm cross --help # status=0; match=/Usage: gvm cross \[option\] <os> <arch> \[sub-option\] <file ...>/
+gvm cross -l # status=0; match=/Go cross compile platforms/
+gvm cross --list # status=0; match=/Go cross compile platforms/
+gvm cross --list --porcelain # status=0; match!=/Go cross compile platforms/
+
+## Setup expectation
+
+## 1.8.1
+gvm uninstall go1.8.2 > /dev/null 2>&1
+gvm install go1.8.2 --binary
+
+## Test a cross compile of hello.go
+mkdir -p "${SANDBOX}/cross-test"
+cp "${SANDBOX}/gvm2/tests/gvm_cross_test_input_hello.go" "${SANDBOX}/cross-test"
+
+## Execute command
+result_1="$(builtin cd "${SANDBOX}/cross-test"; __gvm_use "go1.8.2" > /dev/null; "${SANDBOX}/gvm2/scripts/cross" "linux" "386" "gvm_cross_test_input.go" > /dev/null; echo $?)"
+
+## Evaluate result
+[[ "${result_1}" -eq "0" ]] # status=0
