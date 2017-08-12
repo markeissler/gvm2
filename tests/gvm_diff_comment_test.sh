b1b349e318f16aa7563376e3c7149629bd3cffa0
Mark Eissler <mark@mixtur.com>
2017-08-12 00:21:49 -0700

    GVM2-18 #comment Refactor scripts/diff.

---
+++ tests/gvm_diff_comment_test.sh
@@ -0,0 +1,21 @@
+source "${SANDBOX}/gvm2/scripts/gvm"
+
+##
+## Test output messages
+##
+
+gvm diff -h # status=0; match=/Usage: gvm diff \[option\] \[<version>\]/
+gvm diff --help # status=0; match=/Usage: gvm diff \[option\] \[<version>\]/
+
+## Setup expectation
+
+## 1.8.1
+gvm uninstall go1.8.2 > /dev/null 2>&1
+gvm install go1.8.2 --binary
+
+## Test without any changes
+gvm diff go1.8.2 # status=0; match=/Clean/
+
+## Test with changes
+touch "${SANDBOX}/gvm2/gos/go1.8.2/abcd12345afg.test"
+gvm diff go1.8.2 # status=1; match=/\*Dirty\*/
